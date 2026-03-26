ee38086f34c39657ce55fd410ac7f28eaa7446ec
lucasrls98 <lucasrls98@gmail.com>
2026-03-25 22:57:07 -0300

    adding apiLogin command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,16 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('apiLogin', (email, password) => {
+  cy.request({
+    method: 'POST',
+    url: `${Cypress.env('apiUrl')}/login`,
+    body: {
+      email: email,
+      password: password
+    },
+    failOnStatusCode: false 
+  }).then((response) => {
+    if (response.status === 200) {
+      Cypress.env('bearerToken', response.body.authorization);
+    }
+    return response;
+  });
+});
