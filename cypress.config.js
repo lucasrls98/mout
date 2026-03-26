6aa9a5a0acaaba39595284202e9ae048badcbec9
lucasrls98 <lucasrls98@gmail.com>
2026-03-25 22:43:39 -0300

    cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: 'https://front.serverest.dev', // URL do Frontend 
+    env: {
+      apiUrl: 'https://serverest.dev' // URL da API 
     },
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+    reporter: 'cypress-mochawesome-reporter',
+    video: false, // Otimização de performance
+    screenshotOnRunFailure: true
   },
 });
